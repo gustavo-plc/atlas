9a75ebb25f35e73c81a61ccf1dbf386a94c9bb10
Gustavo Cunha <124817057+gustavo-plc@users.noreply.github.com>
2023-03-25 19:41:19 +0000

    Deletei arquivos em excesso

--- .ccls-cache/@home@runner@Lista-1-APC-II/main.c
+++ .ccls-cache/@home@runner@Lista-1-APC-II/main.c
@@ -5,7 +5,7 @@ int main() {
   printf("A variável n é iniciada com o valor: %d\n", n);
   int *p_n = &n;
   *p_n = 0;
-  printf("Após manipulação com o ponteiro, a variável n se tornou: %d\n", n);
+  printf("Após manipulação com o ponteiro, a variável n se tornou: %d\n\n", n);
   
   return 0;
 }
