8a851dd073efe85142e2ffbd9a744b7560c84a5f
Gustavo Cunha <124817057+gustavo-plc@users.noreply.github.com>
2023-03-25 19:38:34 +0000

    Exercício 1

--- .ccls-cache/@home@runner@Lista-1-APC-II/main.c
+++ .ccls-cache/@home@runner@Lista-1-APC-II/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
+int main() {
+  int n = 8;
+  printf("A variável n é iniciada com o valor: %d\n", n);
+  int *p_n = &n;
+  *p_n = 0;
+  printf("Após manipulação com o ponteiro, a variável n se tornou: %d\n", n);
+  
   return 0;
 }
